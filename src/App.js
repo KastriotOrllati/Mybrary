3cc3a3351402ba48938ca363f561bf5f8e17c353
bledon ibishi <bi47191@ubt-uni.net>
2021-09-14 14:20:00 +0200

    added post categories logic

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ function App() {
             <AdminPage />
           </Route>
           <Route path="/">
+            <Navbar />
+
             <Homepage />
           </Route>
         </Switch>
@@ -29,6 +31,5 @@ function App() {
 export default App;
 
 // <Route path="/signin">
-// <Navbar />;
 //   <SignInRegister />
 // </Route>
