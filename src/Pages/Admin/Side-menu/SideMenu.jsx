9412406c7975baf6f21a22d72be309dc85ff05ec
ahalili.911@gmail.com <ahalili.911@gmail.com>
2021-09-27 22:50:50 +0200

    Added logout to admin

--- src/Pages/Admin/Side-menu/SideMenu.jsx
+++ src/Pages/Admin/Side-menu/SideMenu.jsx
@@ -1,8 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import "./Sidemenu.css";
 
+import { authHeader } from "../../../Utils/authHeader";
+import { getUser } from "../../../Utils/getUser";
+
 export default function SideMenu() {
+  const history = useHistory();
+  const [login, setLogin] = useState(false);
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    let response = authHeader();
+
+    if (response.Authorization) {
+      setLogin(true);
+    }
+    let user = getUser();
+    setUser(user);
+    console.log("Side menu login");
+  }, [login]);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("user");
+    history.push("/");
+
+    history.go();
+  };
+
   return (
     <div className="side-menu">
       <div className="menu-container">
@@ -19,6 +45,11 @@ export default function SideMenu() {
         <div className="links">
           <Link to="/admin/add-admin">Add Admin</Link>
         </div>
+        {login ? (
+          <div className="log-out">
+            <button onClick={handleLogout}>LogOut</button>
+          </div>
+        ) : null}
       </div>
     </div>
   );
